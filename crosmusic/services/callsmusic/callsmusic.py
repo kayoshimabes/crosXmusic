929beab83a143314271bfec953ae4806ea6a9c9a
glitterneey <91388336+GLITTERneey@users.noreply.github.com>
2021-11-07 16:27:22 +0700

    Update callsmusic.py

--- crosmusic/services/callsmusic/callsmusic.py
+++ crosmusic/services/callsmusic/callsmusic.py
@@ -18,8 +18,8 @@ from typing import Dict
 
 from pytgcalls import GroupCallFactory
 
-from DaisyXMusic.services.callsmusic import client
-from DaisyXMusic.services.queues import queues
+from crosmusic.services.callsmusic import client
+from crosmusic.services.queues import queues
 
 instances: Dict[int, GroupCallFactory] = {}
 active_chats: Dict[int, Dict[str, bool]] = {}
