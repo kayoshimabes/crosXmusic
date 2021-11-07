1fb32e0a5567facd9e683394f15468e9e65c91f9
glitterneey <91388336+GLITTERneey@users.noreply.github.com>
2021-11-07 17:39:06 +0700

    Update gcast.py

--- crosmusic/modules/gcast.py
+++ crosmusic/modules/gcast.py
@@ -20,8 +20,8 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from DaisyXMusic.config import SUDO_USERS
-from DaisyXMusic.services.callsmusic.callsmusic import client as USER
+from crosmusic.config import SUDO_USERS
+from crosmusic.services.callsmusic.callsmusic import client as USER
 
 
 @Client.on_message(filters.command(["broadcast"]))
