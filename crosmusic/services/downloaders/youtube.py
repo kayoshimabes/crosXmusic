404562516d147e7d772a10faeefd1f35b8eee256
glitterneey <91388336+GLITTERneey@users.noreply.github.com>
2021-11-07 16:54:32 +0700

    Update youtube.py

--- crosmusic/services/downloaders/youtube.py
+++ crosmusic/services/downloaders/youtube.py
@@ -20,8 +20,8 @@ from os import path
 
 from youtube_dl import YoutubeDL
 
-from DaisyXMusic.config import DURATION_LIMIT
-from DaisyXMusic.helpers.errors import DurationLimitError
+from crosmusic.config import DURATION_LIMIT
+from crosmusic.helpers.errors import DurationLimitError
 
 ydl_opts = {
     "format": "bestaudio/best",
