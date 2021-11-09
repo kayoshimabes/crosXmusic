57d3939f0df206fcce6223d9c2a3cc3785ec548a
glitterneey <91388336+GLITTERneey@users.noreply.github.com>
2021-11-10 01:47:49 +0700

    Ok

--- crosmusic/modules/ytsearch.py
+++ crosmusic/modules/ytsearch.py
@@ -39,15 +39,15 @@ async def ytsearch(_, message: Message):
             await message.reply_text("/search needs an argument!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Searching....")
+        m = await message.reply_text("sᴇᴀʀᴄʜɪɴɢ....")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
         while i < 4:
-            text += f"Title - {results[i]['title']}\n"
-            text += f"Duration - {results[i]['duration']}\n"
-            text += f"Views - {results[i]['views']}\n"
-            text += f"Channel - {results[i]['channel']}\n"
+            text += f"ᴛɪᴛʟᴇ - {results[i]['title']}\n"
+            text += f"ᴅᴜʀᴀᴛɪᴏɴ - {results[i]['duration']}\n"
+            text += f"ᴠɪᴇᴡs - {results[i]['views']}\n"
+            text += f"ᴄʜᴀɴɴᴇʟ - {results[i]['channel']}\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
